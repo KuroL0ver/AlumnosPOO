8f854fe4cb62baf3945599796b8546e2faf81883
Camila Fimbres <camifimbres@gmail.com>
2024-11-25 08:02:57 -0700

    Agregar archivos de proyecto.

---
+++ AlumnosPOO/Alumno.cs
@@ -0,0 +1,45 @@
+﻿namespace AlumnosPOO
+{
+    internal class Alumno
+    {
+        string nombre;
+        string grupo;
+        string contraseña;
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+
+            }
+
+        }
+    }
+    public string Grupo
+    {
+        get
+        {
+            return grupo;
+        }
+        set
+        {
+            grupo = value;
+        }
+    }
+    public string Contraseña
+    {
+        get
+        {
+            return contraseña;
+        }
+        set 
+        {
+            contraseña = value;
+        }
+    }
+}
---
+++ AlumnosPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AlumnosPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Alumno morita = new Alumno();
+            morita.Nombre = "Santiago Mora";
+            morita.grupo = "DGDIE11";
+            morita.contraseña = "abc1233";
+
+            Console.WriteLine(morita.contraseña);
+        }
+    }
+}
